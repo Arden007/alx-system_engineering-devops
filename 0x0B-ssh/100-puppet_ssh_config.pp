00cd2d47b1248da06ad15b294919a3d69b3b92fa
Arden007 <cloveryuno664@gmail.com>
2023-03-28 22:58:44 +0200

    0x0C & 0x0D

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # Configure ssh file to connect to server without password using puppet scripting
 include stdlib
 
-file_line { 'Turn off password auth':
+file_line { 'Turn off passwd auth':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'Identity file':
+file_line { 'Delare Identity file':
   ensure => 'present',
   path => '/etc/ssh/ssh_config'
   line => '    IdentityFile ~/.ssh/school',
