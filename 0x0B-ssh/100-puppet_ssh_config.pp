2c3aada7361d5d13b39504539ebc923aa2dd8128
Arden007 <cloveryuno664@gmail.com>
2023-03-28 13:01:10 +0200

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,16 @@
 # Configure ssh file to connect to server without password using puppet scripting
+include stdlib
+
 file_line { 'Turn off password auth':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => '    PasswordAuthentication no',
+  replace => true,
 }
 
 file_line { 'Identity file':
   ensure => 'present',
   path => '/etc/ssh/ssh_config'
   line => '    IdentityFile ~/.ssh/school',
+  replace => true,
 }
