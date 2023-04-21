c5a0b90956cb7aa71ae81797aaf1e0a08d38af9c
Arden007 <cloveryuno664@gmail.com>
2023-04-21 11:31:09 +0200

    0x12 complete

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,10 +5,12 @@ file_line { 'Turn off passwd auth':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => '    PasswordAuthentication no',
+  replace => true,
 }
 
 file_line { 'Delare Identity file':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => '    IdentityFile ~/ssh-key/ssh-key',
+  replace => true,
 }
