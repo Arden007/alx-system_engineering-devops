faff902417577a19baf2e6485728dc19060e353c
Arden007 <cloveryuno664@gmail.com>
2023-05-11 14:58:44 +0200

    0x16 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,13 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Returns:
-         number of subscribers (not active users, total subscribers) for a given subreddit.
-         If an invalid subreddit is given, the function should return 0.
+         number of subscribers (total subscribers).
+         If an invalid subreddit return 0.
     """
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 400:
-        return (0)
-    return (response.json().get("data").get("subscribers"))
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return (0)
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,19 +4,19 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed for a given subreddit. 
+    """ prints the titles of the first 10 hot posts listed for a given subreddit.
         If not a valid subreddit, print None.
     """
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {'limit': 10}
     response = requests.get(url, headers=headers, allow_redirects=False,
                             params=params)
 
-    if response.status_code == 400:
-        print(None) 
-        return   
-    else:
+    if response.status_code == 200:
         titles = response.json().get('data').get('children')
         for title in titles:
             print(title.get('data').get('title'))
+    else:
+        print(None)
+        return
