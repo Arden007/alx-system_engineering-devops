eb814d56650c42d961e1164ac0988a32b89e7b58
Arden007 <cloveryuno664@gmail.com>
2023-05-11 14:59:47 +0200

    0x16 fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ after = None
 def recurse(subreddit, hot_list=[]):
     """ Returns a list of titles of all hot posts on a given subreddit. """
     global after
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {'after': after}
     response = requests.get(url, headers=headers, allow_redirects=False,
