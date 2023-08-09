88aab58ac56581ed522d32793fc99c116873e62e
Arden007 <cloveryuno664@gmail.com>
2023-08-09 13:44:18 +0200

    fail

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,18 +2,22 @@
 """
 Reddit Top Ten Hot Posts
 
-This script queries the Reddit API to retrieve the titles of the first 10 hot posts for a given subreddit.
+This script queries the Reddit API to retrieve the titles of the first 10
+ hot posts for a given subreddit.
 If the subreddit is not valid or does not exist, the script prints None.
 """
 
 import requests
 
+
 def top_ten(subreddit):
     """
-    Retrieve and print the titles of the first 10 hot posts for a given subreddit.
+    Retrieve and print the titles of the first 10 hot posts for a given 
+    subreddit.
 
-    This function queries the Reddit API using the '/hot.json' endpoint to obtain the titles
-    of the first 10 hot posts from the specified subreddit.
+    This function queries the Reddit API using the '/hot.json' endpoint
+    to obtain the titles of the first 10 hot posts from the specified 
+    subreddit.
 
     Args:
         subreddit (str): The name of the subreddit.
