89bf66d9fdc3212d1c8280d9950a7406323a594f
Arden007 <cloveryuno664@gmail.com>
2023-08-10 17:37:25 +0200

    pip8 fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -31,12 +31,11 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {'limit': 10}
     response = requests.get(url, headers=headers,
-                            allow_redirects=False, params=params)
+                            allow_redirects=False,params=params)
 
     if response.status_code == 200:
         data = response.json()
         posts = data['data']['children'][:10]
-
         for post in posts:
             print(post['data']['title'])
     else:
