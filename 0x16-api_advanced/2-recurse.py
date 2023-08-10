7f09a385524c7244480f200439289abe7aab1d55
Arden007 <cloveryuno664@gmail.com>
2023-08-10 17:11:09 +0200

    remove comment

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -23,31 +23,24 @@ def recurse(subreddit, hot_list=[]):
         list or None: A list containing titles of hot posts,
         or None if the subreddit is invalid or an error occurs.
     """
-    # Global variable to keep track of the 'after' parameter for pagination
     global after
     after = None
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {'after': after}
 
-    # Making a GET request to the Reddit API
     response = requests.get(url, headers=headers, allow_redirects=False, params=params)
 
     if response.status_code == 200:
-        # Extracting 'after' parameter for pagination
         data = response.json().get('data').get('after')
         if data is not None:
             after = data
-            # Recursive call to continue fetching posts
             recurse(subreddit, hot_list)
 
-        # Extracting post titles from the response
         titles = response.json().get('data').get('children')
         for title in titles:
             hot_list.append(title.get('data').get('title'))
 
-        # Returning the list of hot post titles
         return hot_list
     else:
-        # Returning None in case of an invalid subreddit or other errors
         return None
