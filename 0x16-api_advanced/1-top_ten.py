dc42f476de50ffe7e1921133781e92dee4938a8c
Arden007 <cloveryuno664@gmail.com>
2023-08-10 17:46:11 +0200

    pip8 fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,10 +4,9 @@ Reddit Top Ten Hot Posts
 
 This script queries the Reddit API to retrieve
 the titles of the first 10 hot posts for a given 
-subreddit.If the subreddit is not valid or does
+subreddit. If the subreddit is not valid or does
 not exist, the script prints None.
 """
-
 
 import requests
 
@@ -31,7 +30,7 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {'limit': 10}
     response = requests.get(url, headers=headers,
-                            allow_redirects=False,params=params)
+                            allow_redirects=False, params=params)
 
     if response.status_code == 200:
         data = response.json()
